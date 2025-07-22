052ce6f008b48048c6420a61c97e4945edb6691b
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-07-22 14:18:21 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace HospitalSystem
+{
+    internal class Program
+    {
+        //create doctor class 
+        class Doctors
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Specialization { get; set; }
+           public DateTime AvailabileAppointment { get; set; }
+            public Doctors(string name, string specialization, int experience)
+            {
+                Id = Id;
+                Name = name;
+                Specialization = specialization;
+                AvailabileAppointment = DateTime.Now.AddDays(7); // Example: next available appointment in 7 days
+           
+            }
+            public void DisplayInfo()
+            {
+                Console.WriteLine("Doctor ID: " + Id);
+                Console.WriteLine("Name: " + Name);
+                Console.WriteLine("Specialization: " + Specialization);
+                Console.WriteLine("Available Appointment: " + AvailabileAppointment.ToString("dd/MM/yyyy"));
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
