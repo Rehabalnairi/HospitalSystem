4de7083e7a928dbeddbd1bb3bf1f6a97861bcbb4
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-07-22 14:22:17 +0400

    create patient class

--- Program.cs
+++ Program.cs
@@ -8,14 +8,14 @@
             public string Id { get; set; }
             public string Name { get; set; }
             public string Specialization { get; set; }
-           public DateTime AvailabileAppointment { get; set; }
+            public DateTime AvailabileAppointment { get; set; }
             public Doctors(string name, string specialization, int experience)
             {
                 Id = Id;
                 Name = name;
                 Specialization = specialization;
                 AvailabileAppointment = DateTime.Now.AddDays(7); // Example: next available appointment in 7 days
-           
+
             }
             public void DisplayInfo()
             {
@@ -25,10 +25,35 @@
                 Console.WriteLine("Available Appointment: " + AvailabileAppointment.ToString("dd/MM/yyyy"));
 
             }
-        }//
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+            // create patient class
+            class Patients
+            {
+                public string Id { get; set; }
+                public string Name { get; set; }
+                public int Age { get; set; }
+                public string Phone { get; set; }
+
+                public Patients(string name, int age, string phone)
+                {
+                    Id = Id;
+                    Name = name;
+                    Age = age;
+                    Phone = phone;
+
+                }
+                public void DisplayInfo()
+                {
+                    Console.WriteLine("Patient ID: " + Id);
+                    Console.WriteLine("Name: " + Name);
+                    Console.WriteLine("Age: " + Age);
+                    Console.WriteLine("Phone: " + Phone);
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, World!");
+            }
         }
     }
-}
+} 
